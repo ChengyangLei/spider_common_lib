ed9b69fc5d1687a4795b11c0ba4197e74ab341df
xianglong <xianglong@xiangongpc>
2015-03-17 10:29:08 +0800

    增加爬虫的ssl支持

---
+++ spider_common_lib/src/main/test/test/TestSSL.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+
+import org.epiclouds.handlers.util.MyX509TrustManager;
+
+
+public class TestSSL {
+	public static void main(String args[]) throws Exception{
+		 // 创建SSLContext对象，并使用我们指定的信任管理器初始化
+        TrustManager[] tm = { new MyX509TrustManager() };
+        SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+        sslContext.init(null, tm, new java.security.SecureRandom());
+        // 从上述SSLContext对象中得到SSLSocketFactory对象
+        SSLSocketFactory ssf = sslContext.getSocketFactory();
+        // 创建URL对象
+        URL myURL = new URL("https://www.baidu.com");
+        // 创建HttpsURLConnection对象，并设置其SSLSocketFactory对象
+        HttpsURLConnection httpsConn = (HttpsURLConnection) myURL.openConnection();
+        httpsConn.addRequestProperty("host", "www.baidu.com");
+        httpsConn.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36");
+        httpsConn.setSSLSocketFactory(ssf);
+        // 取得该连接的输入流，以读取响应内容
+        InputStreamReader insr = new InputStreamReader(httpsConn.getInputStream());
+        // 读取服务器的响应内容并显示
+        int respInt = insr.read();
+        while (respInt != -1) {
+            System.out.print((char) respInt);
+            respInt = insr.read();
+        }
+	}
+}
